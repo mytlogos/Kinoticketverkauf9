e2c576e5a5d552908f7ef9bc8abdded1f57a315b
mytlogos <mytlogos@hotmail.com>
2019-07-02 13:48:47 +0200

    Geldbetrag implementation
    - geldbetrag und test implementiert.
    - Fehler in methoden addierbar, multiplizierbar & subtrahierbar, etc...

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -1,57 +1,130 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Fachwert für Geldbeträge
  *
  */
-public class Geldbetrag {
+public final class Geldbetrag {
 
-	private int _geldbetrag;
+	private final int _eurocent;
 	
 	private Geldbetrag(int euroCent)
 	{
-		_geldbetrag = euroCent;
+		_eurocent = euroCent;
 	}
 	
 	
 	public int getEuro()
 	{
-		return _geldbetrag / 100;
+		return _eurocent / 100;
 	}
 	
 	
 	public int getCent()
 	{
-		return _geldbetrag % 100;
+		return _eurocent % 100;
 	}
 	
 	
-	public getFormatierterString()
+	public String getFormatierterString()
 	{
 		return getEuro() + "," + getCent() + "€";
 	}
 	
 	
-	public addiere(Geldbetrag summand)
+	public Geldbetrag addiere(Geldbetrag summand)
 	{
-		return new Geldbetrag(this._geldbetrag + summand._geldbetrag);
+		assert addierbar(summand):"Integer-overflow, vorbedingung verletzt";
+		return new Geldbetrag(this._eurocent + summand._eurocent);
 	}
 	
 	
 	/**
 	 * @param subtrahend subtrahend wird vom eigentlichen Geldbetrag abgezogen
 	 */
-	public subtrahiere(Geldbetrag subtrahend)
+	public Geldbetrag subtrahiere(Geldbetrag subtrahend)
 	{
-		return new Geldbetrag(this._geldbetrag - summand._geldbetrag);
+		assert subtrahierbar(subtrahend):"Integer-overflow, vorbedingung verletzt";
+		return new Geldbetrag(this._eurocent - subtrahend._eurocent);
 	}
 	
 	
-	public mulipliziere(int faktor)
-	
-		return new Geldbetrag(this._geldbetrag * faktor);
+	public Geldbetrag multipliziere(int faktor)
+	{
+		assert multiplizierbar(faktor):"Integer-overflow, vorbedingung verletzt";
+		return new Geldbetrag(this._eurocent * faktor);
 	}
 	
 	
-	public get
+	public static Geldbetrag get(int eurocent)
+	{
+		return new Geldbetrag(eurocent);
+	}
+	
+	
+	public static Geldbetrag get(String eurocentString)
+	{
+		Matcher matcher = Pattern.compile("(-?)(\\d{1,8})(,(\\d\\d))?").matcher(eurocentString);
+		if(!matcher.matches()) {
+			throw new IllegalArgumentException();
+		}
+		int eurocent = Integer.parseInt(matcher.group(2)) * 100;
+		if (matcher.group(4) != null) 
+		{
+			eurocent += Integer.parseInt(matcher.group(4));	
+		}
+		
+		if("-".equals(matcher.group(1)))
+		{
+			eurocent *= -1;
+		}
+		
+		return new Geldbetrag(eurocent);
+	}
+	
+	
+	public boolean multiplizierbar(int faktor)
+	{
+		if(Integer.MAX_VALUE / faktor > this._eurocent)
+		{
+			return true;
+		}
+		if(Integer.MIN_VALUE / faktor < this._eurocent && faktor < 0)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public boolean addierbar(Geldbetrag geldbetrag)
+	{
+		return Integer.MAX_VALUE - geldbetrag._eurocent <= _eurocent;
+	}
+	
+	
+	public boolean subtrahierbar(Geldbetrag geldbetrag)
+	{
+		return Integer.MAX_VALUE + geldbetrag._eurocent <= _eurocent;
+	}
+	
+	
+	public boolean equals(Object obj)
+	{
+		if(!(obj instanceof Geldbetrag))
+		{
+			return false;
+		}
+		return this._eurocent == ((Geldbetrag)obj)._eurocent;
+	}
+	
+	
+	public int hashCode()
+	{
+		return _eurocent;
+	}
 }
--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -17,9 +17,9 @@ public class GeldbetragTest
     	
         assertEquals(0, p.getEuro());
         assertEquals(0, p.getCent());
-        assertEquals(10, p2.getEuro());
+        assertEquals(1, p2.getEuro());
         assertEquals(5, p2.getCent());     
-        assertEquals(10, p3.getEuro());
+        assertEquals(1, p3.getEuro());
         assertEquals(5, p3.getCent());
     }
     
@@ -31,8 +31,8 @@ public class GeldbetragTest
     	
     	Geldbetrag summe = p.addiere(p2);
     	
-        assertEquals(3, p.getEuro());
-        assertEquals(15, p.getCent());
+        assertEquals(3, summe.getEuro());
+        assertEquals(15, summe.getCent());
     }
     
     @Test
@@ -43,20 +43,19 @@ public class GeldbetragTest
     	
     	Geldbetrag summe = p.subtrahiere(p2);
     	
-        assertEquals(0, p.getEuro());
-        assertEquals(105, p.getCent());
+        assertEquals(1, summe.getEuro());
+        assertEquals(5, summe.getCent());
     }
     
     @Test
     public void testGeldbetragProdukt()
     {
     	Geldbetrag p = Geldbetrag.get(210);
-    	Geldbetrag p2 = Geldbetrag.get(105);
     	
-    	Geldbetrag summe = p.multipliziere(p2);
+    	Geldbetrag summe = p.multipliziere(105);
     	
-        assertEquals(220, p.getEuro());
-        assertEquals(50, p.getCent());
+        assertEquals(220, summe.getEuro());
+        assertEquals(50, summe.getCent());
     }
     
     
@@ -66,10 +65,13 @@ public class GeldbetragTest
     {
     	Geldbetrag p = Geldbetrag.get("0");
     	Geldbetrag p2 = Geldbetrag.get("101,05");
+    	Geldbetrag p3 = Geldbetrag.get("-101,05");
         assertEquals(0, p.getEuro());
         assertEquals(0, p.getCent());
         assertEquals(101, p2.getEuro());
         assertEquals(5, p2.getCent());
+        assertEquals(-101, p3.getEuro());
+        assertEquals(-5, p3.getCent());
     }
 
     @Test
@@ -77,13 +79,47 @@ public class GeldbetragTest
     {
         Geldbetrag p1 = Geldbetrag.get(1);
         Geldbetrag p2 = Geldbetrag.get(1);
-        Geldbetrag p3 = Geldbetrag.get(3001); // Sitz unterschiedlich
-        Geldbetrag p4 = Geldbetrag.get(3201); // Reihe unterschiedlich
+        Geldbetrag p3 = Geldbetrag.get(3001);
+        Geldbetrag p4 = Geldbetrag.get(3201);
+        Geldbetrag p5 = Geldbetrag.get(-3201);
+        Geldbetrag p6 = Geldbetrag.get(-3201);
 
-        assertTrue(p1.equals(p2));
-        assertTrue(p1.hashCode() == p2.hashCode());
+        assertFalse(p1.equals(null));
+        
+        assertEquals(p1, p2);
+        assertEquals(p1.hashCode(), p2.hashCode());
 
         assertFalse(p1.equals(p3));
         assertFalse(p1.equals(p4));
+        
+        assertEquals(p5, p6);
+        assertEquals(p5.hashCode(), p6.hashCode());
+    }
+    
+    
+    @Test
+    public void testAddierbar()
+    {
+    	Geldbetrag p1 = Geldbetrag.get(Integer.MAX_VALUE);
+    	Geldbetrag p2 = Geldbetrag.get(Integer.MIN_VALUE);
+    	Geldbetrag p3 = Geldbetrag.get(1);
+    	
+    	assertFalse(p1.addierbar(p1));
+    	assertTrue(p2.addierbar(p3));
+    	assertTrue(p1.addierbar(p2));
+    	assertTrue(p3.addierbar(p3));
+    }
+    
+    
+    @Test
+    public void testSubtrahierbar()
+    {
+    	Geldbetrag p1 = Geldbetrag.get(Integer.MAX_VALUE);
+    	Geldbetrag p2 = Geldbetrag.get(Integer.MIN_VALUE);
+    	Geldbetrag p3 = Geldbetrag.get(1);
+    	
+    	assertFalse(p1.subtrahierbar(p2));
+    	assertFalse(p2.subtrahierbar(p3));
+    	assertTrue(p1.subtrahierbar(p1));
     }
 }
